cd815065813f48ad72bfc7fd5a2f34c5dca9f3b8
Jim Sebastian Ramirez Lopez <jsrlopez@uniempresarial.edu.co>
2024-11-05 13:52:26 -0500

    configuraciones iniciales

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -7,11 +7,24 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SistemaVenta.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
 
 
 namespace SistemaVenta.IOC
 {
     public static class Dependencia
     {
+
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration) 
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
     }
 }
