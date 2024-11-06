28e632f5c76c6d063fcef6d1fd1f6e1da871d1cc
Jim Sebastian Ramirez Lopez <jsrlopez@uniempresarial.edu.co>
2024-11-06 09:16:41 -0500

    Primeras plantillas del html agregadas uwu

---
+++ SistemaVenta.AplicacionWeb/Controllers/DashBoardController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class DashBoardController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaVenta.AplicacionWeb/Controllers/NegocioController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class NegocioController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
