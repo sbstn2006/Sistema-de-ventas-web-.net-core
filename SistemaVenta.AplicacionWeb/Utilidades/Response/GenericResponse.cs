35c875a069c6e2b8fa32d393ba54370039b4f714
Jim Sebastian Ramirez Lopez <jsrlopez@uniempresarial.edu.co>
2024-11-07 16:32:59 -0500

    Muchos cambios

---
+++ SistemaVenta.AplicacionWeb/Utilidades/Response/GenericResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaVenta.AplicacionWeb.Utilidades.Response
+{
+    public class GenericResponse<TObject>
+    {
+        public int Estado { get; set; }
+        public string? Mensaje { get; set; }
+        public TObject? Objeto { get; set; }
+        public List<TObject>? ListaObjeto { get; set; }
+
+
+
+    }
+}
--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -27,10 +27,15 @@ namespace SistemaVenta.IOC
             });
 
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
             services.AddScoped<IVentaRepository, VentaRepository>();
 
             services.AddScoped<ICorreoService,CorreoService>();
+            services.AddScoped<IFireBaseService,FireBaseService>();
+            services.AddScoped<IUtilidadesService,UtilidadesService>();
+            services.AddScoped<IRolService,RolService>();
+
+
+
 
         }
     }
