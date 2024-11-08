9bc82138805fe26245ba28fe25fa2164ebd0efbf
Jim Sebastian Ramirez Lopez <jsrlopez@uniempresarial.edu.co>
2024-11-08 17:02:29 -0500

    Algunos viewModels, aun faltan

---
+++ SistemaVenta.AplicacionWeb/Models/ViewModels/VMVenta.cs
@@ -0,0 +1,32 @@
+﻿using SistemaVenta.Entity;
+
+namespace SistemaVenta.AplicacionWeb.Models.ViewModels
+{
+    public class VMVenta
+    {
+        public int IdVenta { get; set; }
+
+        public string? NumeroVenta { get; set; }
+
+        public int? IdTipoDocumentoVenta { get; set; }
+        public int? TipoDocumentoVenta { get; set; }
+
+        public int? IdUsuario { get; set; }
+        public int? Usuario { get; set; }
+
+        public string? DocumentoCliente { get; set; }
+
+        public string? NombreCliente { get; set; }
+
+        public decimal? SubTotal { get; set; }
+
+        public decimal? ImpuestoTotal { get; set; }
+
+        public decimal? Total { get; set; }
+
+        public DateTime? FechaRegistro { get; set; }
+
+        public virtual ICollection<VMDetalleVenta> DetalleVenta { get; set; }
+
+    }
+}
---
+++ SistemaVenta.AplicacionWeb/Utilidades/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,119 @@
+﻿using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.Entity;
+using System.Globalization;
+using AutoMapper;
+
+
+
+
+namespace SistemaVenta.AplicacionWeb.Utilidades.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            #region Rol
+            CreateMap<Rol, VMRol>().ReverseMap();
+            #endregion Rol
+
+            #region Usuario
+            CreateMap<Usuario, VMUsuario>()
+                .ForMember(destino =>
+                    destino.EsActivo,
+                    opt => opt.MapFrom(origen => origen.EsActivo == true ? 1 : 0)
+                    )
+                    .ForMember(destino =>
+                    destino.NombreRol,
+                    opt => opt.MapFrom(origen => origen.IdRolNavigation.Descripcion) 
+                    );
+
+            CreateMap<VMUsuario, Usuario>()
+                .ForMember(destino =>
+                destino.EsActivo,
+                opt => opt.MapFrom(origen => origen.EsActivo == 1 ? true : false)
+                )
+                .ForMember(destino =>
+                destino.IdRolNavigation,
+                opt => opt.Ignore()
+                );
+
+            #endregion Usuario
+
+            #region Negocio
+
+            CreateMap<Negocio, VMNegocio>()
+                .ForMember(destino =>
+                destino.PorcentajeImpuesto,
+                opt => opt.MapFrom(origen => Convert.ToString(origen.PorcentajeImpuesto.Value, new CultureInfo("es-CO")))
+                
+                );
+
+            CreateMap<VMNegocio, Negocio>()
+                .ForMember(destino =>
+                destino.PorcentajeImpuesto,
+                opt => opt.MapFrom(origen => Convert.ToDecimal(origen.PorcentajeImpuesto, new CultureInfo("es-CO")))
+
+                );
+
+
+
+
+            #endregion Negocio
+
+            #region Categoria
+
+            CreateMap<Categoria, VMCategoria>()
+                .ForMember(destino =>
+                destino.EsActivo,
+                opt => opt.MapFrom(origen => origen.EsActivo == true ? 1:0)
+
+                );
+
+            CreateMap<VMCategoria, Categoria>()
+                .ForMember(destino =>
+                destino.EsActivo,
+                opt => opt.MapFrom(origen => origen.EsActivo == 1 ? true:false)
+
+                );
+
+            #endregion Categoria
+
+            #region Producto
+
+            CreateMap<Producto, VMProducto>()
+                .ForMember(destino =>
+                destino.EsActivo,
+                opt => opt.MapFrom(origen => origen.EsActivo == true ? 1 : 0)
+                )
+
+                .ForMember(destino =>
+                destino.NombreCategoria,
+                opt => opt.MapFrom(origen => origen.IdCategoriaNavigation.Descripcion)
+                )
+                .ForMember(destino =>
+                destino.Precio,
+                opt => opt.MapFrom(origen => Convert.ToString(origen.Precio.Value, new CultureInfo("es-CO")))
+                );
+
+
+            CreateMap<VMProducto,Producto>()
+                .ForMember(destino =>
+                destino.EsActivo,
+                opt => opt.MapFrom(origen => origen.EsActivo == 1 ? true : false)
+                )
+
+                .ForMember(destino =>
+                destino.IdCategoriaNavigation,
+                opt => opt.Ignore()
+                )
+                .ForMember(destino =>
+                destino.Precio,
+                opt => opt.MapFrom(origen => Convert.ToDecimal(origen.Precio, new CultureInfo("es-CO")))
+                );
+            #endregion Producto
+
+
+        }
+
+    }
+}
